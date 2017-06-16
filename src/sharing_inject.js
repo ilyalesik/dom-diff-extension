2db25589092ccebe844cab5cbe65ef3d772afb80
Ilia Lesik <ilialesik@gmail.com>
2017-06-16 17:55:15 +0300

    console.log diff

--- src/sharing_inject.js
+++ src/sharing_inject.js
@@ -1,23 +1,41 @@
-import $ from 'jquery'
-import jsdiff from 'diff';
+import $ from 'jquery';
+var jsdiff = require('diff');
 
 let intervalId;
 function run() {
+    if (intervalId) {
+        stop();
+        return;
+    }
     let prevHtml = $('body').html();
     intervalId = setInterval(()=>{
+
         let nextHtml = $('body').html();
 
-        if (nextHtml !== prevHtml) {
+        if (nextHtml != prevHtml) {
             let diff = jsdiff.diffChars(prevHtml, nextHtml);
-            console.log(diff);
+            let text = '';
+            let colors = [];
+            diff.forEach(function(part){
+                // green for additions, red for deletions
+                // grey for common parts
+                var color = part.added ? 'green' :
+                    part.removed ? 'red' : 'grey';
+                    text += '%c' + part.value;
+                    colors.push(`color: ${color}`);
+
+            });
+            let result = [text, ...colors];
+            console.log(...result);
         }
 
         prevHtml = nextHtml;
-    }, 100);
+    }, 500);
 }
 
 function stop() {
     clearInterval(intervalId);
+    intervalId = undefined;
 }
 
 chrome.extension.onMessage.addListener(
