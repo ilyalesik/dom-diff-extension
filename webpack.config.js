f29279c4000dee3b085e047f96061d9179d0edb4
Ilia Lesik <ilialesik@gmail.com>
2017-06-16 16:17:01 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path')
+
+module.exports = {
+    debug: true,
+    devtool: 'source-map',
+    entry:   {
+        inject: './src/sharing_inject.js',
+        popup: './src/popup.js',
+        background: './src/background.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: "[name]-bundle.js",
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
